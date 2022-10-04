1173c32ed80ba933de9b12bdcb731ea6a5cd6339
Peter luque <Peterluque21@gmail.com>
2022-10-04 10:48:49 -0400

    remove array an dlist projects

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        private int v1;
+        private string v2;
+        private string v3;
+        private string v4;
+        private string v5;
+        private bool v6;
+
+        public Car()
+        {
+        }
+
+        public Car(int v1, string v2, string v3, string v4, string v5, bool v6)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+            this.v5 = v5;
+            this.v6 = v6;
+        }
+
+        public string Make { get; set; 
+        public int Year { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public string IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string MakeEngineNoise)
+        {
+            Console.WriteLine(MakeEngineNoise);
+        }
+
+        public void MakeHonkNoise(string honkNoise)
+        {
+            Console.Write(honkNoise);
+        }
+
+        internal void MakeEngineNoise()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    
+    
+
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,9 +1,20 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Xml.Schema;
 
 namespace CarLotSimulator
 {
     class Program
     {
+        private static object lot;
+
+        public static string Make { get; private set; }
+        public static int Year { get; private set; }
+        public static string Model { get; private set; }
+        public static string EngineNoise { get; private set; }
+        public static string HonkNoise { get; private set; }
+        public static string IsDriveable { get; private set; }
+
         static void Main(string[] args)
         {
             //TODO
@@ -17,8 +28,34 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            var pennyCar = new Car();
+            pennyCar.Make = "RTX";
+            pennyCar.Year = 2013;
+            pennyCar.Model = "Dodge Charger";
+            pennyCar.IsDriveable = "Yes";
+            pennyCar.EngineNoise = "no";
+            pennyCar.HonkNoise = "Yes";
 
-            //*************BONUS*************//
+            var freddyCar = new Car();
+            {
+                Make = "tesla";
+                Year = 2019;
+                Model = "Cyber Truck";
+                EngineNoise = "...";
+                HonkNoise = "not loud";
+                IsDriveable = "Yes";
+
+            };
+
+            var marlonCar = new Car(2013, "Honda", "Civic", "Vroom", "Vrugga", true);
+
+            
+
+            pennyCar.MakeEngineNoise();
+            freddyCar.MakeEngineNoise();
+            marlonCar.MakeEngineNoise();
+
+
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
@@ -28,6 +65,7 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
-        }
-    }
-}
+        }   
+    
+
+
